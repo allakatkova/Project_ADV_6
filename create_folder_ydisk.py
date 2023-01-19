acd1a260db8b4e0fd8e6c296052249369de7d79b
Alla Katkova <allakatkova@bk.ru>
2023-01-19 22:43:28 +0500

    Task 2 complited

---
+++ create_folder_ydisk.py
@@ -0,0 +1,21 @@
+import requests
+
+TOKEN = 'y0_AgAAAAACEB5lAADLWwAAAADVOwmof3Hnr59kQIKqqaFV0AmPgB22Kro'
+HEADERS = {'Content-Type': 'application/json',
+           'Accept': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
+URL = 'https://cloud-api.yandex.net/v1/disk/resources'
+
+
+def check_folder(folder_name):
+    result = requests.get(f'{URL}?path={folder_name}', headers=HEADERS)
+    return result.status_code
+
+
+def create_folder(folder_name):
+    result = requests.put(f'{URL}?path={folder_name}', headers=HEADERS)
+    return result.status_code
+
+
+# if __name__ == '__main__':
+#     create_folder('test')
+#     check_folder('test')
---
+++ test_task_2.py
@@ -0,0 +1,19 @@
+import create_folder_ydisk as cfyd
+from unittest.case import TestCase
+
+FOLDER_NAME = 'folder_test'
+
+
+class TestYandexFolder(TestCase):
+
+    def test_folder_check(self):
+        self.assertTrue(cfyd.check_folder(FOLDER_NAME) == 404,
+                        f'Папка {FOLDER_NAME} создана на Я.Диске')
+
+    def test_folder_create(self):
+        self.assertTrue(cfyd.create_folder(FOLDER_NAME) == 201,
+                        f'Папка {FOLDER_NAME} не найдена на Я.Диске')
+
+
+# if __name__ == '__main__':
+#     TestYandexFolder()
